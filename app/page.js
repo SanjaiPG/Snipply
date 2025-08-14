f969e053f5a8074f1e9690bab13acfb5522d4cd2
SanjaiPG <sanjaigukesan8686@gmail.com>
2025-08-14 15:16:21 +0530

    changes for the view

--- app/page.js
+++ app/page.js
@@ -2,9 +2,6 @@ import PDFViewer from "./components/PDFViewer";
 
 export default function Home() {
   return (
-    <main style={{ padding: "2rem" }}>
-      <h1>Adobe PDF Embed Example</h1>
-      <PDFViewer />
-    </main>
+    <PDFViewer />
   );
 }
